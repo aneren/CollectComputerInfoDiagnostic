54c260174f809cbe08dda96c47c6f97882b008a8
aneren <loewenkampp@gmail.com>
2023-01-21 16:36:03 -0500

    Add initial auto-elevate

--- CollectComputerInfo.ps1
+++ CollectComputerInfo.ps1
@@ -19,7 +19,16 @@ TO ADD:
 
 #>
 
-$ScriptVer = "1.2"
+$ScriptVer = "1.3"
+
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+     $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+     Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+     Exit
+    }
+}
 
 #######################
 # CREATE LOG FILE DIRECTORIES
